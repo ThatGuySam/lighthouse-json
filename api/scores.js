c3474591c31ff57f930efec5953cf0a79144e4ee
Sam Carlton <sam@sam.lc>
2020-07-25 14:33:29 -0500

    Allow all origins

--- api/scores.js
+++ api/scores.js
@@ -57,5 +57,9 @@ export default async function (req, res) {
         }
     }))
 
+    // Set Cors Headers to allow all origins so data can be requested by a browser
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    
     res.json(urlScores)
 }
